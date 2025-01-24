25188191259e5d61af2048709e472dcdd1028961
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-01-24 20:47:54 +0100

    editor

--- src/components/editor/Viewport/Sidebar/SidebarItem.tsx
+++ src/components/editor/Viewport/Sidebar/SidebarItem.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import { styled } from 'styled-components';
+import React from "react";
+import { styled } from "styled-components";
 
-import Arrow from '../../../../public/icons/arrow.svg';
+const Arrow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 6">
+      <title>ChevronDownMedium</title>
+      <rect id="ToDelete" fill="#ff13dc" opacity="0" />
+      <path d="M9.99,1.01A.9999.9999,0,0,0,8.28266.30327L5,3.58594,1.71734.30327A.9999.9999,0,1,0,.30327,1.71734L4.29266,5.69673a.99965.99965,0,0,0,1.41468,0L9.69673,1.71734A.99669.99669,0,0,0,9.99,1.01Z" />
+    </svg>
+  );
+};
 
 const SidebarItemDiv = styled.div<{ $visible?: boolean; $height?: string }>`
   height: ${(props) =>
-    props.$visible && props.$height && props.$height !== 'full'
+    props.$visible && props.$height && props.$height !== "full"
       ? `${props.$height}`
-      : 'auto'};
+      : "auto"};
   flex: ${(props) =>
-    props.$visible && props.$height && props.$height === 'full'
+    props.$visible && props.$height && props.$height === "full"
       ? `1`
-      : 'unset'};
+      : "unset"};
   color: #545454;
 `;
 
@@ -59,7 +67,7 @@ export const SidebarItem: React.FC<SidebarItemProps> = ({
           if (onChange) onChange(!visible);
         }}
         className={`cursor-pointer bg-white border-b last:border-b-0 flex items-center px-2 ${
-          visible ? 'shadow-sm' : ''
+          visible ? "shadow-sm" : ""
         }`}
       >
         <div className="flex-1 flex items-center">
@@ -68,7 +76,7 @@ export const SidebarItem: React.FC<SidebarItemProps> = ({
           <h2 className="text-xs uppercase">{title}</h2>
         </div>
         <Chevron $visible={visible}>
-          <img src={Arrow} alt="Arrow" />
+          <Arrow />
         </Chevron>
       </HeaderDiv>
       {visible ? (
