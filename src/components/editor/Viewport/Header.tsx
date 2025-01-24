872d280a9703552c9c9a84f3df3d9a51cab77bf1
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-01-24 20:19:01 +0100

    editor

---
+++ src/components/editor/Viewport/Header.tsx
@@ -0,0 +1,108 @@
+import { useEditor } from '@craftjs/core';
+import { Tooltip } from '@material-ui/core';
+import cx from 'classnames';
+import { styled } from 'styled-components';
+
+import Customize from '../../../public/icons/customize.svg';
+import RedoSvg from '../../../public/icons/toolbox/redo.svg';
+import UndoSvg from '../../../public/icons/toolbox/undo.svg';
+
+const HeaderDiv = styled.div`
+  width: 100%;
+  height: 45px;
+  z-index: 99999;
+  position: relative;
+  padding: 0px 10px;
+  background: #d4d4d4;
+  display: flex;
+`;
+
+const Btn = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 5px 15px;
+  border-radius: 3px;
+  color: #fff;
+  font-size: 13px;
+  svg {
+    margin-right: 6px;
+    width: 12px;
+    height: 12px;
+    fill: #fff;
+    opacity: 0.9;
+  }
+`;
+const Checkmark = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+      <title>S Checkmark 18 N</title>
+      <rect id="Canvas" fill="#ff13dc" opacity="0" width="18" height="18" />
+      <path className="a" d="M15.656,3.8625l-.7275-.5665a.5.5,0,0,0-.7.0875L7.411,12.1415,4.0875,8.8355a.5.5,0,0,0-.707,0L2.718,9.5a.5.5,0,0,0,0,.707l4.463,4.45a.5.5,0,0,0,.75-.0465L15.7435,4.564A.5.5,0,0,0,15.656,3.8625Z" />
+    </svg>
+  );
+};
+const Item = styled.a<{ disabled?: boolean }>`
+  margin-right: 10px;
+  cursor: pointer;
+  svg {
+    width: 20px;
+    height: 20px;
+    fill: #707070;
+  }
+  ${(props) =>
+    props.disabled &&
+    `
+    opacity:0.5;
+    cursor: not-allowed;
+  `}
+`;
+
+export const Header = () => {
+  const { enabled, canUndo, canRedo, actions } = useEditor((state, query) => ({
+    enabled: state.options.enabled,
+    canUndo: query.history.canUndo(),
+    canRedo: query.history.canRedo(),
+  }));
+
+  return (
+    <HeaderDiv className="header text-white transition w-full">
+      <div className="items-center flex w-full px-4 justify-end">
+        {enabled && (
+          <div className="flex-1 flex">
+            <Tooltip title="Undo" placement="bottom">
+              <Item disabled={!canUndo} onClick={() => actions.history.undo()}>
+                <img src={UndoSvg} alt="Undo" />
+              </Item>
+            </Tooltip>
+            <Tooltip title="Redo" placement="bottom">
+              <Item disabled={!canRedo} onClick={() => actions.history.redo()}>
+                <img src={RedoSvg} alt="Redo" />
+              </Item>
+            </Tooltip>
+          </div>
+        )}
+        <div className="flex">
+          <Btn
+            className={cx([
+              'transition cursor-pointer',
+              {
+                'bg-green-400': enabled,
+                'bg-primary': !enabled,
+              },
+            ])}
+            onClick={() => {
+              actions.setOptions((options) => (options.enabled = !enabled));
+            }}
+          >
+            {enabled ? (
+              <Checkmark />
+            ) : (
+              <img src={Customize} alt="Customize" />
+            )}
+            {enabled ? 'Finish Editing' : 'Edit'}
+          </Btn>
+        </div>
+      </div>
+    </HeaderDiv>
+  );
+};
